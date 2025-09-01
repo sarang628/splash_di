fa0c295829004553ff3b8060be4a83c31643ae7c
sryang <sarang628@gmail.com>
2025-09-01 08:05:57 +0800

    change package

--- SplashModule.kt
+++ SplashModule.kt
@@ -3,7 +3,7 @@ package com.sryang.splash.di.splash_di
 import com.sryang.splash.usecase.SplashUseCase
 import com.sarang.torang.api.ApiLogin
 import com.sarang.torang.api.handle
-import com.sarang.torang.data.dao.LoggedInUserDao
+import com.sarang.torang.core.database.dao.LoggedInUserDao
 import com.sarang.torang.session.SessionService
 import dagger.Module
 import dagger.Provides
